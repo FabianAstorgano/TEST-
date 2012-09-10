b8abc47821c0fd93916e8017016f8724359724de
Fabian Astorgano <mil1nueve9sesenta6ynueve9@hotmail.com>
2012-09-10 00:37:20 -0300

    HEROKU

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lajuanita::Application.config.secret_token = '1224f59aec13d215d289d3f079abf0fdac9f793fc52fa425f737d141671898cd30f932a4561be5abe04b80eac82550647502c5847d51276c60a465fb009c1c62'
